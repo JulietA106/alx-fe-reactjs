92c2697dcf30e68bc1159753d8623b82ad9adfad
Juliet Angaandi <julietangaandi93@gmail.com>
2025-12-13 10:15:24 +0000

    Move React Query setup to App.jsx for checker

--- react-query-demo/src/App.jsx
+++ react-query-demo/src/App.jsx
@@ -1,12 +1,20 @@
 import React from "react";
 import PostsComponent from "./components/PostsComponent.jsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+// Create QueryClient instance
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <div className="App">
-      <h1>React Query Demo</h1>
-      <PostsComponent />
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <div className="App">
+        <h1>React Query Demo</h1>
+        <PostsComponent />
+      </div>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 }
 
